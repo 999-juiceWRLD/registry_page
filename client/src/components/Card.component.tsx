f97c2f3d77d6d868ba294a27490b1f3d676d2cae
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 00:43:25 +0300

    center content

--- client/src/components/Card.component.tsx
+++ client/src/components/Card.component.tsx
@@ -2,9 +2,11 @@ import { Form } from "./Form.component";
 
 export const Card: React.FunctionComponent = () => {
     return (
-        <div className="my-10 mx-auto w-[75%] text-center border-2 border-[#64CCC5]">
-            <h4>Sign up</h4>
-            <Form />
+        <div className="my-10 mx-auto w-[60%] border-2 rounded border-[#64CCC5]">
+            <div className="flex flex-col content-center items-center gap-y-5 my-5">
+                <h4>Sign up</h4>
+                <Form />
+            </div>
         </div>
     );
 }
