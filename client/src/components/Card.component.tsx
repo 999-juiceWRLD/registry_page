4c32ba352c19d5f546f08a92bc5af658208b3858
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 00:25:03 +0300

    import `<Form />` componenet

--- client/src/components/Card.component.tsx
+++ client/src/components/Card.component.tsx
@@ -1,7 +1,10 @@
+import { Form } from "./Form.component";
+
 export const Card: React.FunctionComponent = () => {
     return (
         <div className="my-10 mx-auto w-[75%] text-center border-2 border-[#64CCC5]">
             <h4>Sign up</h4>
+            <Form />
         </div>
     );
 }
