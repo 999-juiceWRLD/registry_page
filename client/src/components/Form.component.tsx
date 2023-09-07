7458bf8f7ea72ea01a2578b8942292223c91a6fc
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-08 00:51:36 +0300

    some imports

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -1,6 +1,14 @@
 import { Button } from "./Button.component";
+// import { useState } from "react";
+import { IFormData } from "../interfaces/FormData.interface";
+// import axios from "axios";
 
-export const Form: React.FunctionComponent = () => {
+export const Form: React.FunctionComponent<IFormData> = () => {
+    
+    // const [values, setValues] = useState<IFormData>({ emailAddress: '', password: '' });
+    
+    // don't forget onChange and onClick
+
     return (
         <div className="w-full max-w-xs">
             <form action="" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -11,12 +19,12 @@ export const Form: React.FunctionComponent = () => {
                     <input type="text" className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="" className="block text-gray-700 text-sm font-bold mb-2">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
                     <input type="password" className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
-                <Button />
+                <Button buttonType="button" onClick={() => { console.log('button is clicked') }}/>
             </form>
         </div>
     );
