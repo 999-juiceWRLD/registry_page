2cec3bf84f6024601cdb5ec9273f0b85d579fa82
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 23:26:17 +0300

    change bg-color name hex to specified name & remove unnecessary upper margin

--- client/src/components/Card.component.tsx
+++ client/src/components/Card.component.tsx
@@ -2,7 +2,7 @@ import { Form } from "./Form.component";
 
 export const Card: React.FunctionComponent = () => {
     return (
-        <div className="my-10 mx-auto w-[60%] border-2 rounded border-[#64CCC5]">
+        <div className="mx-auto w-[60%] border-2 rounded border-light-blue-border">
             <div className="flex flex-col content-center items-center gap-y-5 my-5">
                 <h4>Sign up</h4>
                 <Form />
