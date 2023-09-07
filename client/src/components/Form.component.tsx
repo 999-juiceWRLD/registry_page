c2ded23da1e36196a35ab5312214ea0defe39701
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 22:59:13 +0300

    add input to email address bar

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -3,7 +3,10 @@ export const Form: React.FunctionComponent = () => {
         <div className="w-full max-w-xs">
             <form action="" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                        Email Address
+                    </label>
+                    <input type="text" className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
             </form>
         </div>
