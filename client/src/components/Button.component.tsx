b3d7ac52b3c3e1792f6a593cc58daae06d61bc57
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 23:37:07 +0300

    create button component

---
+++ client/src/components/Button.component.tsx
@@ -0,0 +1,7 @@
+export const Button: React.FunctionComponent = () => {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded py-2 px-5">
+            Sign in
+        </button>
+    );
+}
