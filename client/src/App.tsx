cc6a065cfa495940b3ed53b536e4a5d0f1fc2bad
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 23:27:48 +0300

    change bg-color hex to it's specified name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,12 @@ import './App.css'
 function App() {
 
   return (
-    <div className='h-screen bg-[#053B50] text-white'>
+    <div className='h-screen bg-dark-blue-plane text-white'>
         <div className='flex flex-col justify-center'>
-          <div className='my-10 bg-[#053B50]'>
+          <div className='my-10 bg-dark-blue-plane'>
             <Info />
           </div>
-          <div className='bg-[#053B50]'>
+          <div className='bg-dark-blue-plane'>
             <Card />
           </div>
         </div>
