b16326184f83109065099bede30422fdd4ab8550
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-07 23:23:17 +0300

    set default colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
 export default {
   content: [
     './index/html',
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'dark-blue-plane': '#053B50',
+      'light-blue-border': '#64CCC5'
+    },
     extend: {},
   },
   plugins: [],
