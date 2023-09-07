67091d56be252e7688ab0bdee925b1695594b3c2
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-08 00:22:39 +0300

    add new parameter for `<Button />` component

--- client/src/components/Button.component.tsx
+++ client/src/components/Button.component.tsx
@@ -1,6 +1,9 @@
-export const Button: React.FunctionComponent = () => {
+import { IButtonType } from "../interfaces/ButtonType.interface";
+
+export const Button: React.FunctionComponent<IButtonType> = ({ buttonType, onClick }: IButtonType) => {
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded py-2 px-5">
+        <button type={buttonType} onClick={onClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold rounded py-2 px-5">
             Sign in
         </button>
     );
