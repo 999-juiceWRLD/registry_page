0d60a69d894d09171dfcb677e25ad9a4233bf1cb
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-08 21:39:41 +0300

    make post request successfuly, and return jwt token

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -1,22 +1,26 @@
 import { Button } from "./Button.component";
 import { useState } from "react";
 import { IFormData } from "../interfaces/FormData.interface";
-// import axios from "axios";
+import axios from "axios";
 
 export const Form: React.FunctionComponent<IFormData> = () => {
     
-    const [values, setValues] = useState<IFormData>({ emailAddress: '', password: '' });
+    const [values, setValues] = useState<IFormData>({ email: '', password: '' });
     
-    // don't forget onChange and onClick
-
     const changeValues = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [e.target.name]: e.target.value }); 
     };
 
-    const log = () => {
-        console.log(values.emailAddress);
-        console.log(values.password);
+    const dataRequest = async () => {
+        axios.defaults.withCredentials = true;
+        await axios.post('http://localhost:3000/api/sign-up', values)
+            .then(res => { console.log(res.data) })
+            .catch(err => { console.log(err) });
     }
+
+    // const log = () => {
+    //     console.log(values);
+    // }
 
     return (
         <div className="w-full max-w-xs">
@@ -25,7 +29,7 @@ export const Form: React.FunctionComponent<IFormData> = () => {
                     <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
                         Email Address
                     </label>
-                    <input type="text" name="emailAddress" onChange={changeValues} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
+                    <input type="text" name="email" onChange={changeValues} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -33,7 +37,7 @@ export const Form: React.FunctionComponent<IFormData> = () => {
                     </label>
                     <input type="password" name="password" onChange={changeValues} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
-                <Button buttonType="button" onClick={log}/>
+                <Button buttonType="button" onClick={dataRequest}/>
             </form>
         </div>
     );
