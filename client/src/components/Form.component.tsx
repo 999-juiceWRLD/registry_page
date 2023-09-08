0c6b850317d38d082169c3f78300d64ee0260be6
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-08 20:21:42 +0300

    fix error, finally can print anything entered

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -1,13 +1,25 @@
 import { Button } from "./Button.component";
-// import { useState } from "react";
+import { useState } from "react";
 import { IFormData } from "../interfaces/FormData.interface";
 // import axios from "axios";
 
 export const Form: React.FunctionComponent<IFormData> = () => {
     
-    // const [values, setValues] = useState<IFormData>({ emailAddress: '', password: '' });
+    const [values, setValues] = useState<IFormData>({ emailAddress: '', password: '' });
     
     // don't forget onChange and onClick
+
+    const onChange = (e) => {
+        setValues({
+          ...values,
+          [e.target.name]: e.target.value
+        }); 
+    };
+
+    const log = () => {
+        console.log(values.emailAddress);
+        console.log(values.password);
+    }
 
     return (
         <div className="w-full max-w-xs">
@@ -16,15 +28,15 @@ export const Form: React.FunctionComponent<IFormData> = () => {
                     <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
                         Email Address
                     </label>
-                    <input type="text" className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
+                    <input type="text" name="emailAddress" onChange={onChange} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
-                    <input type="password" className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
+                    <input type="password" name="password" onChange={onChange} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
                 </div>
-                <Button buttonType="button" onClick={() => { console.log('button is clicked') }}/>
+                <Button buttonType="button" onClick={log}/>
             </form>
         </div>
     );
