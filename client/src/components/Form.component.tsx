df5d2cc4d2344962663c8f4d42e5608e2c2edc51
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-09 23:13:22 +0300

    use email and password components instead of raw code

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -1,6 +1,8 @@
 import { Button } from "./Button.component";
 import { useState } from "react";
 import { IFormData } from "../interfaces/FormData.interface";
+import { Email } from "./Email.component";
+import { Password } from "./Password.component";
 import axios from "axios";
 
 export const Form: React.FunctionComponent<IFormData> = () => {
@@ -25,18 +27,14 @@ export const Form: React.FunctionComponent<IFormData> = () => {
     return (
         <div className="w-full max-w-xs">
             <form action="" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-                        Email Address
-                    </label>
-                    <input type="text" name="email" onChange={changeValues} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
-                </div>
-                <div className="mb-6">
+                <Email changeValues={changeValues} />
+                <Password changeValues={changeValues} />
+                {/* <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
                     <input type="password" name="password" onChange={changeValues} className="shadow border text-gray-700 p-1 text-sm focus:outline-none focus:shadow-outline" />
-                </div>
+                </div> */}
                 <Button buttonType="button" onClick={dataRequest}/>
             </form>
         </div>
