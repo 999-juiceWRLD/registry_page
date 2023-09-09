63d75888ac6552da5340ad863173ef026b08c9f2
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-10 01:55:55 +0300

    that's the home page after signing up

---
+++ client/src/routes/home/Home.component.tsx
@@ -0,0 +1,9 @@
+export const Home: React.FunctionComponent = () => {
+    return (
+        <div className="h-screen flex flex-col bg-dark-blue-plane text-white">
+            <p className="mt-10 ml-10 text-sm">You successfully logged in.</p>
+            <p className="mt-3 ml-10 text-sm">You don't need to do anymore! Your account will be deleted soon.</p>
+            <p className="my-10 ml-10 text-center">get lost 😉</p>
+        </div>
+    );
+}
