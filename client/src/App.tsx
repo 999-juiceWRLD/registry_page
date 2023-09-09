fc5b88eab56be3d5a13ddc9b2b11bf26f9dcd643
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-09 23:56:09 +0300

    comment out this file, the new main page is coming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
-import { Card } from './components/Card.component';
-import { Info } from './components/Info.component';
-import './App.css'
+// import { SignUp } from './components/SignUp.component';
+// import { Info } from './components/Info.component';
+// import './App.css'
 
-function App() {
+// function App() {
 
-  return (
-    <div className='h-screen bg-dark-blue-plane text-white'>
-        <div className='flex flex-col justify-center'>
-          <div className='my-10 bg-dark-blue-plane'>
-            <Info />
-          </div>
-          <div className='bg-dark-blue-plane'>
-            <Card />
-          </div>
-        </div>
-    </div>
-  );
-}
+//   return (
+//     <div className='h-screen bg-dark-blue-plane text-white'>
+//         <div className='flex flex-col justify-center'>
+//           <div className='my-10 bg-dark-blue-plane'>
+//             <Info />
+//           </div>
+//           <div className='bg-dark-blue-plane'>
+//             <SignUp />
+//           </div>
+//         </div>
+//     </div>
+//   );
+// }
 
-export default App
+// export default App
