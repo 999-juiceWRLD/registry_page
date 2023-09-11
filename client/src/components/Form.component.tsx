614a5f514dac62c41be49234aea4853008feadad
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-11 21:24:37 +0300

    add an experimental config object for axios and set a mechanism that if the two passwords don't match, it will log something

--- client/src/components/Form.component.tsx
+++ client/src/components/Form.component.tsx
@@ -7,17 +7,32 @@ import axios from "axios";
 
 export const Form: React.FunctionComponent = () => {
     
-    const [values, setValues] = useState<IFormData>({ email: '', password: '' });
+    //
+    const config = {
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        withCredentials: true
+    }
+    //
+
+    const [values, setValues] = useState<IFormData>({ email: '', password: '', passwordAgain: '' });
     
     const changeValues = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [e.target.name]: e.target.value }); 
     };
 
     const dataRequest = async () => {
-        axios.defaults.withCredentials = true;
-        await axios.post('http://localhost:3000/api/sign-up', values)
-            .then(res => { console.log(res.data) })
-            .catch(err => { console.log(err) });
+        if (values.password === values.passwordAgain) {
+            const { email, password } = values;
+            const toBeSent = { email, password };
+            axios.defaults.withCredentials = true;
+            await axios.post('http://localhost:3000/api/sign-up', toBeSent, config)
+                .then(res => { console.log(res.data) })
+                .catch(err => { console.log(err) });
+        } else {
+            console.log('you better check your passwords');
+        }
     }
 
     return (
@@ -25,8 +40,9 @@ export const Form: React.FunctionComponent = () => {
             <form action="" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <Email changeValues={changeValues} />
                 <Password changeValues={changeValues} />
-
+                <Password changeValues={changeValues} />
                 <Button buttonType="button" onClick={dataRequest}/>
+                <a href="" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 pt-3">Already have an account? Login</a>
             </form>
         </div>
     );
